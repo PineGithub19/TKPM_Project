3210b2869bb10f751f651247d40546fdc7ee5cd6
PineGithub19 <114576524+PineGithub19@users.noreply.github.com>
2025-03-21 22:29:14 +0700

    Config custom layout

--- tkpm/src/routes/router.tsx
+++ tkpm/src/routes/router.tsx
@@ -1,16 +1,20 @@
+import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import { DefaultLayout } from '../layouts';
 import { publicRoutes } from './index';
 
-const withLayout = (Page: React.ComponentType, Layout = DefaultLayout) => (
-    <Layout>
-        <Page />
-    </Layout>
-);
+const withLayout = (Page: React.ComponentType, Layout?: React.ComponentType) => {
+    const AppliedLayout = Layout || DefaultLayout;
+    return (
+        <AppliedLayout>
+            <Page />
+        </AppliedLayout>
+    );
+};
 
 export const router = createBrowserRouter(
     publicRoutes.map((route) => ({
         path: route.path,
-        element: withLayout(route.component),
+        element: withLayout(route.component, route.layout),
     })),
 );
