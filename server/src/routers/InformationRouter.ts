5d9685bcd6a05177e14eadd8bfe9c5948538970b
Phan Van Tai <vantai2004kt@gmail.com>
2025-05-09 20:10:05 +0700

    fix update Information route

--- server/src/routers/InformationRouter.ts
+++ server/src/routers/InformationRouter.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import InformationController from '../controllers/InformationController';
 
+const router = express.Router();
 const informationController = new InformationController();
 
-const router = express.Router();
-
+// Configure routes with appropriate middleware
 router.post('/create', informationController.createNewImagePrompt);
 router.put('/update', informationController.updateImagePrompt);
 router.delete('/delete', informationController.deleteImagePrompt);
